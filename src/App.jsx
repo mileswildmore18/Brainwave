9eaa31a6b7f775a1c873fb2308c76e06b9b1b75b
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-13 15:54:05 -0500

    Added the brainwave logo as well as adding the navbar linking to particular sections imported from constants index file of the website and make it responsive on different size devices.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import ButtonGradient from './assets/svg/ButtonGradient';
-import Button from "./components/button";
-const App = () => {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/header";
 
+const App = () => {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-      {/* Align button */}
-      <Button className= "mt-10">
-        Something
-      </Button>
-    </div>
-    <ButtonGradient />
-    </> 
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        {/* Align button */}
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
   );
 };
 
