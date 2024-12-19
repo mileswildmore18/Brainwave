5d60152f6b443081c0d6a28b21c60c0e69daf1c1
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-19 10:40:44 -0500

    Added a Hero section along with the properties from the section tag to create a attractive statement along with a colourful line. Also made the beginning of the Hero section responsive on all size device screens.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/header";
-
+import Hero from "./components/hero";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         {/* Align button */}
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
