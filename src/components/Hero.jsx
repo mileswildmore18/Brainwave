9bf9512528a7bfb22be382d5a48869221b3de218
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-19 11:14:41 -0500

    Added the image of the robot and the background of the transparent robot for the website and made it responsive on different size windows for different devices as well as adjusting the header on top of the website.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
-import { curve } from "../assets";
+import { curve, heroBackground } from "../assets";
 import Section from "./section";
 import Button from "./button";
+import { robot } from "../assets";
 const Hero = () => {
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
@@ -14,7 +15,7 @@ const Hero = () => {
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with BrainwaveCurve
+            Explore the Possibilities of AI Chatting with { ` `}
             {/* Provide a colourful line underneath the last word in the title */}
             <span className="inline-block relative">
               Brainwave{" "}
@@ -35,6 +36,35 @@ const Hero = () => {
           <Button href="/pricing" white>
             Get Started
           </Button>
+        </div>
+        {/* Add photo of the AI for Brainwave and responsive for different devices */}
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              {/* Adjust the size of the phot of robot on different size screens */}
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                {/* Add image of the robot picture */}
+                <img
+                  src={robot}
+                  className="w-full scale-1.7 translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+              </div>
+            </div>
+          </div>
+          {/* Add background of the robot hero picture adjustable on different screen sizes and appear on top of the main picture */}
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top=[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1880}
+              alt="hero"
+            />
+          </div>
         </div>
       </div>
     </Section>
