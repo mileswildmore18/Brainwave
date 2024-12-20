2fc922baa55ddfe8316db090a83ff2773ef336d0
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-20 13:38:50 -0500

    Added 'Explore more' sections to the benefits along with an arrow. Also added an animation of a picture that appears transparently in the background when user hovers over the benefits. Also added light to very important benefits.

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,9 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./section";
-
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 // Add Benefits section
 const Benefits = () => {
   return (
@@ -25,9 +27,36 @@ const Benefits = () => {
                 <h5 className="h2 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
-                    <img src={item.iconUrl} width={48} height={48} alt={item.title}/>
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
                 </div>
               </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                    {item.imageUrl && (
+                        <img src={item.imageUrl}
+                        width={380}
+                        height={362}
+                        alt={item.title}
+                        className="w-full h-full object-cover"/>
+                    )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
