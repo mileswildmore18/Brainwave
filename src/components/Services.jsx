3bbcaafed30460045b5077489341e9c81bf0f8e9
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-20 16:12:04 -0500

    Added the 2nd photo along with making it responsive on all devices. Also added the title of the 2nd Service and the context of it.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import Section from "./section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
 // Add services to the website
 const Services = () => {
   return (
@@ -42,6 +43,29 @@ const Services = () => {
                   </li>
                 ))}
               </ul>
+            </div>
+            {/* Add a generating AI bar in 1st photo at the bottom in the center */}
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+          {/* Add 2nd content with photo and responsiveness on different size screens and devices */}
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  className="h-full w-hull object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot"
+                />
+              </div>
+              {/* Add service content for 2nd photo */}
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+              <h4 className="h4 mb-4">Photo Editing</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Automatically enhance your photos using our AI app's photo editing feature. Try it now!
+              </p>
+              </div>
             </div>
           </div>
         </div>
