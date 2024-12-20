ceaf4c7be36c634c2c11b6b18fc80814cf57a7cf
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-20 16:41:03 -0500

    Added icons for the video generation with an item selected by default with a border around it.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,12 @@ import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
-import {PhotoChatMessage, Gradient, VideoBar, VideoChatMessage} from "./design/Services"
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 // Add services to the website
 const Services = () => {
   return (
@@ -62,13 +67,42 @@ const Services = () => {
               </div>
               {/* Add service content for 2nd photo */}
               <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-              <h4 className="h4 mb-4">Photo Editing</h4>
-              <p className="body-2 mb-[3rem] text-n-3">
-                Automatically enhance your photos using our AI app&apos;s photo editing feature. Try it now!
-              </p>
+                <h4 className="h4 mb-4">Photo Editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app&apos;s
+                  photo editing feature. Try it now!
+                </p>
               </div>
               {/* Add the Chat message bar */}
               <PhotoChatMessage />
+            </div>
+            {/* Add 3rd content with photo and responsiveness on different size screens and devices */}
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-12 xl:px-8">
+                <h4 className="h4 mb-4">Video Generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world's most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {/* Add Icons from the Brainwave Services */}
+                  {brainwaveServicesIcons.map((item, index) => (
+                    // Add item that is selected by default with border
+                    <li key={index}
+                      className={` rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient rounded-2xl md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                        {/* Add border properties around the selected item to be on the outside */}
+                      <div className={index === 2 ?'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]' : ''}>
+                        <img src={item} width={24} height={24} alt="item"/>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
