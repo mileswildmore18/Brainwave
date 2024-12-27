b494d34833e057073cd1f093d114c369a95c8ca8
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-27 13:27:19 -0500

    Fixed a typo in the Services jsx file on line 23 got mb -5 to mb-5.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,7 +20,7 @@ const Services = () => {
         />
         {/* Make content responsive on different size screens and devices */}
         <div className="relative">
-          <div className="relative z-1 flex items-center h-[39rem] mb -5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               {/* Add image for 1st content with responsiveness to different devices */}
               <img
